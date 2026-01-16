9a4c2e23463db3b7012f872a1983dfa62cd8d368
ropeliai <ropeliai.info@gmail.com>
2026-01-17 02:28:02 +0530

    update

--- backend/payment.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createOrder, verifyPayment } from "./payment.controller.js";
-
-const router = express.Router();
-
-router.post("/create-order", createOrder);
-router.post("/verify-payment", verifyPayment);
-
-export default router;
